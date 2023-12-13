610d9abbc7ec730d3d3e7b302ca71198ed02d959
Dor Elazar <dorelazar07@gmail.com>
2023-12-13 15:53:27 +0200

    removed username from updateFlashCard

--- src/services/flashcardService.ts
+++ src/services/flashcardService.ts
@@ -1,17 +1,24 @@
-import { Database } from 'sqlite3';
-import DatabaseSingleton from '../db/DatabaseSingleton';
-import { Flashcard } from '../types/flashcardInterfaces';
-import { User } from '../types/flashcardInterfaces'
-import { Quiz } from '../types/flashcardInterfaces'
-import { resolve } from 'path';
-import { rejects } from 'assert';
+import { Database } from "sqlite3";
+import DatabaseSingleton from "../db/DatabaseSingleton";
+import { Flashcard } from "../types/flashcardInterfaces";
+import { User } from "../types/flashcardInterfaces";
+import { Quiz } from "../types/flashcardInterfaces";
+import { resolve } from "path";
+import { rejects } from "assert";
 const db: Database = DatabaseSingleton.getInstance();
 
 export const createFlashcard = async (flashcard: Flashcard): Promise<void> => {
-  const { id: id, username: username, question: question, answer: answer, category: category, difficulty_level: difficulty_level } = flashcard;
+  const {
+    id: id,
+    username: username,
+    question: question,
+    answer: answer,
+    category: category,
+    difficulty_level: difficulty_level,
+  } = flashcard;
   return new Promise<void>((resolve, reject) => {
     db.run(
-      'INSERT INTO flashcards (id, username, question, answer, category, difficulty_level) VALUES (?, ?, ?, ?, ?, ?)',
+      "INSERT INTO flashcards (id, username, question, answer, category, difficulty_level) VALUES (?, ?, ?, ?, ?, ?)",
       [id, username, question, answer, category, difficulty_level],
       function (err) {
         if (err) {
@@ -25,12 +32,15 @@ export const createFlashcard = async (flashcard: Flashcard): Promise<void> => {
     );
   });
 };
-export const updateFlashcardbyId = async (id: string, body: Partial<Flashcard>): Promise<void> => {
+export const updateFlashcardbyId = async (
+  id: string,
+  body: Partial<Flashcard>
+): Promise<void> => {
   const { username, question, answer, category, difficulty_level } = body;
   return new Promise<void>((resolve, reject) => {
     db.run(
-      'UPDATE flashcards SET username = ?, question = ?, answer = ?, category = ?, difficulty_level = ? WHERE id = ?',
-      [username, question, answer, category, difficulty_level, id],
+      "UPDATE flashcards SET question = ?, answer = ?, category = ?, difficulty_level = ? WHERE id = ?",
+      [question, answer, category, difficulty_level, id],
       function (err) {
         if (err) {
           console.error(err);
@@ -45,33 +55,33 @@ export const updateFlashcardbyId = async (id: string, body: Partial<Flashcard>):
 };
 export const deleteFlashcardById = async (id: string): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
-    db.run(
-      'DELETE FROM flashcards WHERE id = ?',
-      [id],
-      function (err) {
-        if (err) {
-          console.error(err);
-          reject(err);
-        } else {
-          console.log(`Flashcard deleted with ID: ${id}`);
-          resolve();
-        }
+    db.run("DELETE FROM flashcards WHERE id = ?", [id], function (err) {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(`Flashcard deleted with ID: ${id}`);
+        resolve();
       }
-    );
+    });
   });
 };
-export const getFlashcards = async (username: string, category?: string, difficulty_level?: string): Promise<Flashcard[]> => {
+export const getFlashcards = async (
+  username: string,
+  category?: string,
+  difficulty_level?: string
+): Promise<Flashcard[]> => {
   return new Promise<Flashcard[]>((resolve, reject) => {
-    let query = 'SELECT * FROM flashcards WHERE username = ?';
+    let query = "SELECT * FROM flashcards WHERE username = ?";
     const queryParams = [username];
 
     if (category) {
-      query += ' AND category = ?';
+      query += " AND category = ?";
       queryParams.push(category);
     }
 
     if (difficulty_level) {
-      query += ' AND difficulty_level = ?';
+      query += " AND difficulty_level = ?";
       queryParams.push(difficulty_level);
     }
 
@@ -85,28 +95,35 @@ export const getFlashcards = async (username: string, category?: string, difficu
     });
   });
 };
-export const getFlashcardbyId = async (id: string): Promise<Flashcard | null> => {
+export const getFlashcardbyId = async (
+  id: string
+): Promise<Flashcard | null> => {
   return new Promise<Flashcard | null>((resolve, reject) => {
-    db.get('SELECT * FROM flashcards WHERE id = ?', [id], (err, row: Flashcard) => {
-      if (err) {
-        console.error(err);
-        reject(err);
-      } else {
-        resolve(row);
+    db.get(
+      "SELECT * FROM flashcards WHERE id = ?",
+      [id],
+      (err, row: Flashcard) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          resolve(row);
+        }
       }
-    });
+    );
   });
 };
 // quizzes
 export const generateQuizzes = async (): Promise<Quiz[]> => {
-  return new Promise<Quiz[]>((resolve, reject) => {
-
-  })
+  return new Promise<Quiz[]>((resolve, reject) => {});
 };
 
 // login
-export const validateUser = async (username: string, password: string): Promise<User> => {
-  const userQuery = 'SELECT * FROM user WHERE username = ? AND password = ?';
+export const validateUser = async (
+  username: string,
+  password: string
+): Promise<User> => {
+  const userQuery = "SELECT * FROM user WHERE username = ? AND password = ?";
   return new Promise<User>((resolve, reject) => {
     db.get(userQuery, [username, password], (err, row: User) => {
       if (err) {
@@ -118,20 +135,31 @@ export const validateUser = async (username: string, password: string): Promise<
   });
 };
 
-export const registerUser = async (username: string, password: string, fname: string, lname: string): Promise<boolean> => {
+export const registerUser = async (
+  username: string,
+  password: string,
+  fname: string,
+  lname: string
+): Promise<boolean> => {
   return new Promise<boolean>((resolve, reject) => {
-    db.run(`INSERT INTO User (username, password, fname, lname) VALUES (?, ?, ?, ?)`, [username, password, fname, lname], (err) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(true);
+    db.run(
+      `INSERT INTO User (username, password, fname, lname) VALUES (?, ?, ?, ?)`,
+      [username, password, fname, lname],
+      (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(true);
+        }
       }
-    })
-  })
+    );
+  });
 };
 
-export const getUserFirstName = async (username: string): Promise<string | null> => {
-  const userQuery = 'SELECT fname FROM user WHERE username = ?';
+export const getUserFirstName = async (
+  username: string
+): Promise<string | null> => {
+  const userQuery = "SELECT fname FROM user WHERE username = ?";
   return new Promise<string | null>((resolve, reject) => {
     db.get(userQuery, [username], (err, row: { fname?: string }) => {
       if (err) {
@@ -147,7 +175,7 @@ export const getUserFirstName = async (username: string): Promise<string | null>
   });
 };
 export const userExists = async (username: string): Promise<boolean> => {
-  const userQuery = 'SELECT username FROM user WHERE username = ?';
+  const userQuery = "SELECT username FROM user WHERE username = ?";
   return new Promise<boolean>((resolve, reject) => {
     db.get(userQuery, [username], (err, row: { username?: string }) => {
       if (err) {
@@ -162,6 +190,3 @@ export const userExists = async (username: string): Promise<boolean> => {
     });
   });
 };
-
-
-
